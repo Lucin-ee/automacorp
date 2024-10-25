a5f50988c1cd778c43705a17fb7d4184503f14e5
Lucine SCHIEBER <lucine.schieber@gmail.com>
2024-10-25 15:29:16 +0200

    user & admin

--- src/main/java/com/emse/spring/automacorp/config/SecurityConfig.java
+++ src/main/java/com/emse/spring/automacorp/config/SecurityConfig.java
@@ -2,17 +2,23 @@ package com.emse.spring.automacorp.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class SecurityConfig {
 
     public static final String ROLE_USER = "USER";
+    public static final String ROLE_ADMIN = "ADMIN";
 
     @Bean
     public UserDetailsService userDetailsService() {
@@ -22,6 +28,22 @@ public class SecurityConfig {
         manager.createUser(
                 User.withUsername("user").password(encoder.encode("myPassword")).roles(ROLE_USER).build()
         );
+        manager.createUser(
+                User.withUsername("admin").password(encoder.encode("myPassword")).roles(ROLE_ADMIN).build()
+        );
         return manager;
+    }
+
+    @Bean
+    @Order
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers(AntPathRequestMatcher.antMatcher("/api/**")).hasRole(ROLE_USER)
+                        .anyRequest().permitAll()
+                )
+                .formLogin(withDefaults())
+                .httpBasic(withDefaults())
+                .build();
     }
 }
